abbab81f6989a34359809ceef84e83ffbc27ece6
McGill, Kennard <kennard.mcgill@gatech.edu>
2020-04-26 22:52:23 -0400

    Updated commit with search and pagination

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
+const { Op } = require("sequelize");
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -13,32 +14,72 @@ function asyncHandler(cb){
   }
 }
 
-/* GET Home route. */
-router.get('/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll(); 
-  if(books){
-    res.render("books", { books, title: "Books" });
-  }else{
+/* GET Home route. Pagination*/
+router.get('/', asyncHandler(async (req, res) => { 
+
+const page = parseInt(req.query.page) || 1;  
+const limit =  6;
+const offset = (page - 1) * limit;
+
+const books  = await Book.findAll({
+  offset,
+  limit
+});
+
+const allbooks  = await Book.findAll();
+const totalBooks = allbooks.length;
+const lastpage = Math.ceil(totalBooks / limit);
+
+  if(books){  
+    res.render("books", { books, title: "Books", page, lastpage });
+   }else{
     res.status(404).render("books/not-found", {title: "Page Not Found"});
-  }
+ }
+  
 }));
 
+
 /* GET - Shows full list of books. */
-router.get('/books', asyncHandler(async (req, res) => {
-      res.render("books", {  title: "Books" });
+router.get('/books/:page', asyncHandler(async (req, res) => {
+   
+  res.render("books");
+
 }));
+
 
 /* GET - Shows the Create a new book form. */
 router.get('/new', (req, res) => {
   res.render("books/new", { book: {}, title: "New Book" });
 });
 
+/* GET - Search Term Query - If not return page not found. */
+router.get('/search', asyncHandler(async (req ,res) => {
+  const term = req.query.term;
+  
+  if(term){
+   const books = await Book.findAll({ 
+    where: {
+      [Op.or]: [ 
+        { title: { [Op.like]: '%'+term+'%' } },
+        { author: { [Op.like]: '%'+term+'%' } },
+        { genre: { [Op.like]: '%'+term+'%' } },
+        { year:  { [Op.like]: '%'+term+'%' } }
+        ]
+      }
+   });
+
+  res.render("books/search", { books, title: "Search Results" }); 
+ } else{
+  res.status(404).render("books/not-found", {title: "Page Not Found"});
+ } 
+}));
+
 /* POST - Post a new book to the database. */
 router.post('/new', asyncHandler(async (req, res) => {
   let book;
   try{
       book = await Book.create(req.body);
-      res.redirect("/books/" + book.id);
+      res.redirect("/books");
   } catch(error){
     if(error.name === "SequelizeValidationError"){
       book = await Book.build(req.body);
@@ -53,9 +94,9 @@ router.post('/new', asyncHandler(async (req, res) => {
 router.get('/:id', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   if(book){
-    res.render("books/detail", { book, title: "Update Book"});
+    res.render("books/update", { book, title: "Update Book"});
   } else{
-      res.status(404).render("books/not-found", {title: "Page Not Found"});
+    res.status(404).render("books/not-found", {title: "Page Not Found"});
   }
 }));
 
@@ -66,7 +107,7 @@ router.post("/:id", asyncHandler(async (req, res) => {
     book = await Book.findByPk(req.params.id);
     if(book){
       await book.update(req.body);
-      res.redirect("/books/" + book.id);
+      res.redirect("/books");
     }else {
       res.status(404).render("books/not-found", {title: "Page Not Found"});
     }
@@ -74,7 +115,7 @@ router.post("/:id", asyncHandler(async (req, res) => {
     if(error.name === "SequelizeValidationError"){
       book = await Book.build(req.body);
       book.id = req.params.id;
-      res.render("books/detail", {book, errors: error.errors, title: "Edit Book" })
+      res.render("books/update", {book, errors: error.errors, title: "Edit Book" })
     } else {
       throw error;
     }
@@ -93,6 +134,7 @@ router.post('/:id/delete', asyncHandler(async (req ,res) => {
   }
   
 }));
+
 
 
 
